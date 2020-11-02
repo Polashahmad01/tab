9ced794a64b15037284a60258a093c01ac4ecf67
Polashahmad01 <dev.polashahmad@gmai.com>
2020-11-02 14:51:39 +0600

    Loading Component Finish

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+
 
 const App = () => {
+    const [ loading, setLoading ] = useState(true);
+    // [ jobs, setJobs ] = useState([]);
+    // [ value, setValue ] = useState(0);
+
+    if(loading) {
+        return (
+            <section className="section loading">
+                <h1>Loading...</h1>
+            </section>
+        );
+    }
+
     return <h1>App</h1>
 };
 
